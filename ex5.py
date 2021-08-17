a6f4ede938654bd3030b87a2faa6de3d6852ce3c
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-08-17 11:14:42 +0530

    ex5

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name = 'Nishita'
+my_age = 20 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"she's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
