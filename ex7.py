87a64c341c062e5f89fa3672c7028637d4c0f7f8
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-08-17 11:57:59 +0530

    ex7

---
+++ ex7.py
@@ -0,0 +1,12 @@
+print("Marry has a little lamb")
+print("Its feence was white as {}.".format('snow'))
+print("."*10)
+end1="I"
+end2="s"
+end3="h"
+end4="a"
+end5="K"
+end6="e"
+end7="s"
+print(end1+end2+end3+end4,end=" ")
+print(end5+end6+end7)
