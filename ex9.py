f609aff039b0a998ebdbee21f3517282be653f28
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-08-17 20:15:05 +0530

    ex9

---
+++ ex9.py
@@ -0,0 +1,5 @@
+days="Mon tue wed thus fri sat sun"
+month="\njan\nfeb\nmar\napril\nmay"
+print("Here are the days ",days)
+print("Here are the months ",month)
+print("""There will be no difference while using more than one double quotes""")
