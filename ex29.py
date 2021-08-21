54626f34c517bb3aa6a44c672a2b23586bbfaa9d
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-08-21 20:44:53 +0530

    ex29

---
+++ ex29.py
@@ -0,0 +1,24 @@
+people=20
+cats=30
+dogs=15
+
+if people < cats:
+    print("Too many cats: The worls is doomed")
+
+if people > cats:
+    print("Not many cats: The worls is saved")
+if people < dogs:
+    print("The worls is drooled on")
+if people > dogs:
+    print("The worls is dry")
+
+dogs+=5
+
+if people >= dogs:
+    print("People is greater than equal than dogs")
+
+if people <= dogs:
+    print("People is less than equal than dogs")
+
+if people == dogs:
+    print("People is equal to dogs")
