7a868581cc7a4b5c55c621c08d9b2c48bfd00c51
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-08-21 11:58:00 +0530

    ex19

---
+++ ex19.py
@@ -0,0 +1,15 @@
+def CAC(chesse_count,boxes_of_crackers):
+    print(f"you have {chesse_count} cheese!")
+    print(f" you have {boxes_of_crackers} box of crackers")
+    print("Man that's enough for a party:")
+    print("get a blanket.\n")
+print("We can gust give the function numbers directly!")
+CAC(20,30)
+print("or we can variable from the sript :")
+amt_cheese=10
+amt_cracker=50
+CAC(amt_cheese,amt_cracker)
+print(" We can do math inside too:")
+CAC(10+5,20+5)
+print("And we can combie two , variable and maths:")
+CAC(amt_cheese+10,amt_cracker+20)
