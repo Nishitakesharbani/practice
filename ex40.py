891a9b31278ac2702ad550e7f99dc59efce0f64d
Nishita <nishitakesharbanibkp2001@gmail.com>
2021-10-07 00:35:52 +0530

    ex40

---
+++ ex40.py
@@ -0,0 +1,11 @@
+class mystuff(object):
+    def __init__(self):
+        self.tangeri="And a thousand year between"
+        print(self.tangeri)
+    
+    def apple(self):
+        print("I am classy Apple")
+
+thing=mystuff()
+thing.apple()
+#print(thing.tangeri)
